88391589cbde47594c4ffcc4d484d7ea7ee6bf8a
Yuv Garg <129162262+yuvg03@users.noreply.github.com>
2024-09-19 18:11:18 +0530

    Add files via upload

---
+++ RepoNest_front/src/App.jsx
@@ -0,0 +1,38 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import LikesPage from "./pages/LikesPage"
+import ExplorePage from "./pages/ExplorePage"
+import SignUpPage from "./pages/SignupPage"
+
+import Sidebar from "./components/Sidebar"
+import { Toaster } from "react-hot-toast"
+import { useAuthContext } from "./context/AuthContext"
+
+function App() {
+
+	const {authUser, loading} = useAuthContext();
+	console.log("Authenticated user :", authUser); 
+	if(loading) return null;
+
+  return (
+    <>
+    <div className=' bg-black flex text-stone-100'>
+			<Sidebar />
+			<div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+				<Routes>
+				<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+					<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+					<Route path='/explore' element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
+					<Route path='/likes' element={authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
+				</Routes>
+				<Toaster/>
+			</div>
+		</div>
+    </>
+  )
+}
+
+export default App
